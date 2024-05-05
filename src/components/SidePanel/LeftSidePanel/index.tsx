319b229a7926c938584db984b31ce7c13dfebfb7
Sourav Purkait <souravpurkait@Souravs-MacBook-Pro.local>
2024-05-05 16:20:27 +0530

    hide sidebar on menu click

--- src/components/SidePanel/LeftSidePanel/index.tsx
+++ src/components/SidePanel/LeftSidePanel/index.tsx
@@ -1,11 +1,15 @@
 import UserIcon from "../../../assets/icons/user-icon"
 import WalletIcon from "../../../assets/icons/wallet-icon"
+import { toggleLeftSidePanel } from "../../../store/slices/drawerSlice"
 import { LeftPanelItem, LeftPanelWrapper } from "./styles"
+import { useDispatch } from "react-redux"
 
 const LeftSidePanel = () => {
+    const dispatch = useDispatch();
+    const toggleSidePanel = () => dispatch(toggleLeftSidePanel());
     return (
         <LeftPanelWrapper data-testid="leftsidepanel">
-            <LeftPanelItem active>
+            <LeftPanelItem active onClick={toggleSidePanel}>
                 <UserIcon />
                 Endorsements
             </LeftPanelItem>
