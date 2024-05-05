8b383b9e354ac592420f4fcbf02a51fa5f8eab73
Sourav Purkait <souravpurkait@Souravs-MacBook-Pro.local>
2024-05-05 13:58:52 +0530

    boldness increased

---
+++ src/components/Endorsements/EndorsementInfoLine/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const EndorsementInfoLineContainer = styled.div`
+    display: flex;
+    font-size: 0.675rem;
+    font-weight: 500;
+    align-items: center;
+    margin-top: 1rem;
+    gap: 10px;
+`
