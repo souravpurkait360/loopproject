764d8da00644df45f9831d526e860ab7391b3209
Sourav Purkait <souravpurkait@Souravs-MacBook-Pro.local>
2024-05-05 16:17:43 +0530

    Styles

---
+++ src/components/SidePanel/LeftSidePanel/styles.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const LeftPanelWrapper = styled.div`
+  grid-column: 1;
+  background-color: var(--teal);
+  width: 20vw;
+  max-width: 200px;
+  display: flex;
+  flex-direction: column;
+  padding-top: 20px;
+  @media (max-width: 768px) {
+    width: 100vw;
+    max-width: 100vw;
+    animation: slideIn 0.3s ease-in-out;
+  }
+
+  @keyframes slideIn {
+    from {
+      width: 0vw;
+    }
+    to {
+      width: 100vw;
+    }
+  }
+`;
+
+export const LeftPanelItem = styled.div<{active?: boolean}>`
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+  gap: 10px;
+  background-color: ${({ active }) => (active ? 'var(--light-teal)' : 'transparent')};
+  border-left: 5px solid ${({ active }) => (active ? 'white' : 'transparent')};
+  padding: 10px;
+  padding-left: 30px;
+  cursor: pointer;
+  font-size: 0.675rem;
+  color:  ${({ active }) => (active ? 'rgba(255, 255, 255, 1)' : 'rgba(255, 255, 255, 0.5)')};
+  svg {
+    margin-right: 5px;
+  }
+`;
